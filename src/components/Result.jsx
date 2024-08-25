6ef54759d7f895bc78ad5ce4b038db4d105d847b
Arjun <133730147+hubarjun@users.noreply.github.com>
2024-08-26 04:47:29 +0530

    ui update

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import "../styles/Maincontent.css";
 import Imagepreview from "./Imagepreview";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import { FaRegShareFromSquare } from "react-icons/fa6";
 
-const Result = ({
-  swappedImageUrl,
-  handleShare,
-  handleCopyURL,
-  handleDownload,
-}) => {
+const Result = ({ swappedImageUrl, handleShare, handleDownload }) => {
   return (
     <div className="maincontent">
       <Imagepreview swappedImageUrl={swappedImageUrl} />
       <div className="content">
         <div className="heading-container">
-          <p className="heading">Hop a ride with G.O.A.T</p>
+          <p className="heading">Finally You're with the GOAT</p>
           <p>We know it's your thalapathy too.</p>
         </div>
         <div className="action-buttons">
           <button className="action-button" onClick={handleShare}>
-            Share
+            <FaRegShareFromSquare />
+            <span>Share</span>
           </button>
           <button className="action-button" onClick={handleDownload}>
-            Download
-          </button>
-          <button className="action-button" onClick={handleCopyURL}>
-            Copy URL
+            <IoCloudDownloadOutline />
+            <span>Download</span>
           </button>
         </div>
       </div>
